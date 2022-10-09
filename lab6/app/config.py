20d282bb380382996013c23d46201fdc58b04a37
BASTION8 <tolzvyagin@gmail.com>
2022-10-09 18:58:17 +0300

    обновление 1 лабы + добавление дз на гит

--- lab6/app/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = "4c64ec64c7c848c4d84a89a65e3d38b7a"
-
-SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://std_1687_lab6:mabqteq5s2me@std-mysql.ist.mospolytech.ru'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
